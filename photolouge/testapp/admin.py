8a790aef296effca5bef6b9549d80666391299b2
thefancypant <kkcy92@gmail.com>
2017-08-21 21:40:11 -0400

    model/admin

--- photolouge/testapp/admin.py
+++ photolouge/testapp/admin.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from models import Images
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('imageId','imageType')
+
+
+admin.site.register(Images,ImageAdmin)
